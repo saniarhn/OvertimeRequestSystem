13923eb59e978eddd489306712ef5c44ef17ab94
Faiz <43031284+cilikan@users.noreply.github.com>
2021-12-21 15:54:13 +0700

    first commit

---
+++ OvertimeRequestSystemAPI/VM/RegisterVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OvertimeRequestSystemAPI.VM
+{
+    public class RegisterVM
+    {
+        public int NIP { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        //public string Password { get; set; }
+        public string Position { get; set; }
+
+    }
+}
