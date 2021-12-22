8b27ae082a07ee399c2e8d105808dab69537486a
Faiz <43031284+cilikan@users.noreply.github.com>
2021-12-22 10:32:53 +0700

    adding email2
    adding email2

--- OvertimeRequestSystemAPI/ViewModel/LoginVM.cs
+++ OvertimeRequestSystemAPI/ViewModel/LoginVM.cs
@@ -13,5 +13,6 @@ namespace OvertimeRequestSystemAPI.ViewModel
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+        public string Email2 { get; set; }
     }
 }
